3847569a34c7ed111717b55cb4f6c84b2a06b4bc
Harp1e <github@bplayer.waitrose.com>
2017-08-31 20:18:27 +0100

    L88: Creating a crosshair for our FPS controller

---
+++ FPS Multiplayer/Assets/Scripts/FPS Character/FPSShootingControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSShootingControls : MonoBehaviour 
+{
+    Camera mainCam;
+
+    float fireRate = 15f;
+    float nextTimeToFire = 0f;
+
+    [SerializeField] GameObject concrete_Impact;
+
+	void Start () 
+	{
+        mainCam = Camera.main;
+	}
+	
+	void Update () 
+	{
+        Shoot ();
+	}
+
+    void Shoot ()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time > nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            RaycastHit hit;
+            if (Physics.Raycast (mainCam.transform.position, mainCam.transform.forward, out hit))
+            {
+                Instantiate (concrete_Impact, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+}
